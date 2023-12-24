d9936254966943ab55ebc210b4a9e9538f76a2c1
Aaron Lowe <cyberflameu@gmail.com>
2023-12-24 20:23:25 +1300

    initial changes
    Signed-off-by: Aaron Lowe <cyberflameu@gmail.com>

--- test/server.test.ts
+++ test/server.test.ts
@@ -3,25 +3,28 @@ import { describe, it, beforeEach, afterEach } from 'mocha';
 import {
   InteractionResponseType,
   InteractionType,
-  InteractionResponseFlags,
+  // InteractionResponseFlags,
 } from 'discord-interactions';
 import * as command from '../src/commands.js';
 import sinon from 'sinon';
 import server from '../src/server.js';
+
+const base = 'https://discord.worker.nceahelp.com';
+const applicationId = '1045608895307067453';
 
 describe('Server', () => {
   describe('GET /', () => {
     it('should return a greeting message with the Discord application ID', async () => {
       const request = {
         method: 'GET',
-        url: new URL('/', 'https://discord.worker.nceahelp.com'),
+        url: new URL('/', base),
       };
-      const env = { DISCORD_APPLICATION_ID: '1045608895307067453' };
+      const env = { DISCORD_APPLICATION_ID: applicationId };
 
       const response = await server.fetch(request, env);
       const body = await response.text();
 
-      expect(body).to.equal('👋 1045608895307067453');
+      expect(body).to.equal('👋 ' + applicationId);
     });
   });
 
@@ -43,7 +46,7 @@ describe('Server', () => {
 
       const request = {
         method: 'POST',
-        url: new URL('/interactions', 'https://discord.worker.nceahelp.com'),
+        url: new URL('/interactions', base),
       };
 
       const env = {};
@@ -58,48 +61,81 @@ describe('Server', () => {
       expect(body.type).to.equal(InteractionResponseType.PONG);
     });
 
-    it('should handle a revive command interaction', async () => {
+    // it('should handle a revive command interaction', async () => {
+    //   const interaction = {
+    //     type: InteractionType.APPLICATION_COMMAND,
+    //     data: {
+    //       name: command.REVIVE_COMMAND.name,
+    //     },
+    //   };
+
+    //   const request = {
+    //     method: 'POST',
+    //     url: new URL('/interactions', base),
+    //   };
+
+    //   const env = {};
+
+    //   verifyDiscordRequestStub.resolves({
+    //     isValid: true,
+    //     interaction: interaction,
+    //   });
+
+    //   const response = await server.fetch(request, env);
+    //   const body = await response.json();
+    //   expect(body.type).to.equal(
+    //     InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE
+    //   );
+    // });
+
+    // it('should handle a test command interaction', async () => {
+    //   const interaction = {
+    //     type: InteractionType.APPLICATION_COMMAND,
+    //     data: {
+    //       name: command.TEST_COMMAND.name,
+    //     },
+    //   };
+
+    //   const request = {
+    //     method: 'POST',
+    //     url: new URL('/interactions', base),
+    //   };
+
+    //   const env = {
+    //     DISCORD_APPLICATION_ID: '1045608895307067453',
+    //   };
+
+    //   verifyDiscordRequestStub.resolves({
+    //     isValid: true,
+    //     interaction: interaction,
+    //   });
+
+    //   const response = await server.fetch(request, env);
+    //   const body = await response.json();
+    //   expect(body.type).to.equal(
+    //     InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE
+    //   );
+    //   expect(body.data.content).to.include(
+    //     'https://discord.com/oauth2/authorize?client_id=1045608895307067453&scope=applications.commands+bot&permissions=8'
+    //   );
+    //   expect(body.data.flags).to.equal(InteractionResponseFlags.EPHEMERAL);
+    // });
+
+    it('should handle a ping command interaction', async () => {
       const interaction = {
         type: InteractionType.APPLICATION_COMMAND,
         data: {
-          name: command.REVIVE_COMMAND.name,
+          name: command.PING_COMMAND.name,
         },
       };
 
       const request = {
         method: 'POST',
-        url: new URL('/interactions', 'https://discord.worker.nceahelp.com'),
-      };
-
-      const env = {};
-
-      verifyDiscordRequestStub.resolves({
-        isValid: true,
-        interaction: interaction,
-      });
-
-      const response = await server.fetch(request, env);
-      const body = await response.json();
-      expect(body.type).to.equal(
-        InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE
-      );
-    });
-
-    it('should handle a test command interaction', async () => {
-      const interaction = {
-        type: InteractionType.APPLICATION_COMMAND,
-        data: {
-          name: TEST_COMMAND.name,
-        },
-      };
-
-      const request = {
-        method: 'POST',
-        url: new URL('/interactions', 'https://discord.worker.nceahelp.com'),
+        url: new URL('/interactions', base),
       };
 
       const env = {
-        DISCORD_APPLICATION_ID: '1045608895307067453',
+        DISCORD_APPLICATION_ID: applicationId,
       };
 
       verifyDiscordRequestStub.resolves({
@@ -112,10 +148,8 @@ describe('Server', () => {
       expect(body.type).to.equal(
         InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE
       );
-      expect(body.data.content).to.include(
-        'https://discord.com/oauth2/authorize?client_id=1045608895307067453&scope=applications.commands+bot&permissions=8'
-      );
-      expect(body.data.flags).to.equal(InteractionResponseFlags.EPHEMERAL);
+      expect(body.data.content).to.include('Pong!');
+      // expect(body.data.flags).to.equal(InteractionResponseFlags.EPHEMERAL);
     });
 
     it('should handle an unknown command interaction', async () => {
@@ -128,7 +162,7 @@ describe('Server', () => {
 
       const request = {
         method: 'POST',
-        url: new URL('/interactions', 'https://discord.worker.nceahelp.com'),
+        url: new URL('/interactions', base),
       };
 
       verifyDiscordRequestStub.resolves({
@@ -147,7 +181,7 @@ describe('Server', () => {
     it('should return a "Not Found" response', async () => {
       const request = {
         method: 'GET',
-        url: new URL('/unknown', 'https://discord.worker.nceahelp.com'),
+        url: new URL('/unknown', base),
       };
       const response = await server.fetch(request, {});
       expect(response.status).to.equal(404);
